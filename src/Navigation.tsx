167a8b2ce14e2893879ffaf42c4d0fcaa5e18890
BisiDavi <oludavidconnect@gmail.com>
2021-09-11 14:16:51 +0100

    navbar component added

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from '@pages/Home';
+import menuJson from '@json/menu.json';
+import displayComponents from '@utils/displayComponents';
 
 export default function Navigation(): JSX.Element {
   return (
     <Router>
       <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
+        {menuJson.map((menu) => (
+          <Route key={menu.path} path={menu.path}>
+            {displayComponents(menu.component)}
+          </Route>
+        ))}
       </Switch>
     </Router>
   );
